5a6322c3e05fa69c907720df1456af9c5c4e09ef
Pema Sherpa <pematamang111@gmail.com>
2024-03-13 03:01:47 -0400

    add var in css

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -3,7 +3,7 @@
 document.querySelector("#mode").addEventListener("click", function changeMode(){
     let element = document.body;
     element.classList.toggle("dark-mode");
-    document.querySelector('#portfolio').style.color = "white";
+    document.querySelector('#portfolio').style.color = "blue";
 });
 
 
