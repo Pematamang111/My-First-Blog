f654e3126922bf0572db6d4dca449bad67160034
Pema Sherpa <pematamang111@gmail.com>
2024-03-13 02:42:54 -0400

    application object showing

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -5,7 +5,7 @@ const content = document.querySelector('#content-text');
 const button = document.querySelector('#submit');
 
 //data 
-let data = [] ||  JSON.parse(localStorage.getItem('newDataInput'));
+let data = [] ||  JSON.parse(localStorage.getItem('newData'));
 
 //user interreaction
 button.addEventListener('click', function(event){
@@ -14,17 +14,16 @@ event.preventDefault();
 let newDataInput = {
 username: nameInput.value,
 title: title.value,
-content: content.value
+content: content.value.trim()
 }
 
-localStorage.setItem('newDataInput', JSON.stringify(data));
+localStorage.setItem('newData', JSON.stringify(newDataInput));
 
 if (data === ""){
-    console.log('error');
-    alert("hello");
+    prompt('error');
 }else{
-    data.push('newDataInput');
-    document.createElement('h1').textContent = newDataInput.title;
+    data.push(newDataInput);
+    //document.querySelector('#contents').innerHTML = newDataInput;
     window.location.replace('http://127.0.0.1:5500/blog.html');
 }
 })
